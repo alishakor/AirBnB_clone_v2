aceae51dfd8101f9b5f3b39275592bd6760eea8e
Eseoghene-ODODO <en.ododohotmail.com>
2023-04-19 17:00:36 -0700

    Solution to V2 AirBnB project

--- models/city.py
+++ models/city.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ City module for the HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
     """ City class to store city information """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", backref="cities", cascade="all, delete")
