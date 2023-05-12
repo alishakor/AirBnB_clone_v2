bd016b2295d8159304e43859db0461b0e2aa7c53
alishakor <aliuokeowo@gmail.com>
2023-05-12 16:46:34 +0100

    distributes archive into web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,37 +3,32 @@
 Write a Fabric script (based on the file 1-pack_web_static.py) that
 distributes an archive to your web servers, using the function do_deploy
 """
-from fabric.api import env, run, put
+import os
+from fabric.api import env, put, run, sudo
 from os.path import exists
 
-
+env.user = 'ubuntu'
 env.hosts = ['100.25.163.126', '52.3.240.73']
 
 
 def do_deploy(archive_path):
     """Distribute an archive to the web servers"""
-
-    # Check if the archive file exists
     if not exists(archive_path):
         return False
-    # Get the name of the archive file without the extension
-    archive_filename = archive_path.split("/")[-1]
-    archive_basename = archive_filename.split(".")[0]
-    # Upload the archive to the /tmp/ directory of the web server
-    put(archive_path, '/tmp/' + archive_filename)
-    run("mkdir -p /data/web_static/releases/{}/".format(archive_basename))
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-        .format(archive_filename, archive_basename))
-    # Delete the archive from the web server
-    run("rm /tmp/{}".format(archive_filename))
-    run("mv /data/web_static/releases/{}/web_static/\
-    /data/web_static/releases/{}/"
-        .format(archive_basename, archive_basename))
-    # Remove the empty web_static directory
-    run("rm -rf /data/web_static/releases/{}/web_static"
-        .format(archive_basename))
-    # Delete the symbolic link /data/web_static/current from the web server
-    run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-        .format(archive_basename))
-    return True
+    try:
+        archive_filename = archive_path.split("/")[-1]
+        archive_basename = archive_filename.split(".")[0]
+        folders = '/data/web_static/releases/'
+        put(archive_path, '/tmp/')
+        sudo("mkdir -p folders{}/".format(archive_basename))
+        sudo("tar -xzf /tmp/{} -C folders{}/".format(
+            archive_filename, archive_basename))
+        sudo("rm /tmp/{}".format(archive_filename))
+        sudo("mv folders{0}/web_static/* folders{0}/".format(archive_basename))
+        sudo("rm -rf folders{}/web_static".format(archive_basename))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s folders{}/ /data/web_static/current".format(
+            archive_basename))
+        return True
+    except Exception as e:
+        return False
