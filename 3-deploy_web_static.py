c147d0d0b6254afb7298fb35b49114bf47326fe4
alishakor <aliuokeowo@gmail.com>
2023-05-19 17:49:28 +0100

    web framework task 0 -2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,52 +1,58 @@
 #!/usr/bin/env python3
 """Fabric script that creates and distributes an archive to your web servers"""
-import os.path
-from fabric.api import env, put, run
+import os
+from fabric.api import env, put, sudo, local
 from datetime import datetime
 
-
-env.hosts = ['35.175.132.186', '54.164.162.231']
+env.user = 'ubuntu'
+env.hosts = ['100.25.163.126', '52.3.240.73']
 
 
 def do_pack():
-    """Packs the contents of web_static into a tgz file"""
-    try:
-        if not os.path.exists("versions"):
-            os.mkdir("versions")
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        file_path = "versions/web_static_{}.tgz".format(timestamp)
-        local("tar -cvzf {} web_static".format(file_path))
-        return file_path
-    except:
+    """Archive all the contents of web_static folder"""
+    local("mkdir -p versions")
+    now = datetime.now()
+    timeformat = now.strftime("%Y%m%d%H%M%S")
+
+    # Create the name of the archive
+    archive_name = "web_static_{}.tgz".format(timeformat)
+
+    # Create the path of the archive
+    archive_path = "versions/{}".format(archive_name)
+
+    # Create the archive
+    arch = local("tar -cvzf {} web_static".format(archive_path))
+
+    # Return the path of the archive if it was created successfully
+    if arch:
+        return archive_path
+    else:
         return None
 
-
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers"""
+    """Distribute an archive to the web servers"""
     if not os.path.exists(archive_path):
         return False
     try:
-        file_name = os.path.basename(archive_path)
-        name = file_name.split(".")[0]
-        put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}/".format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(file_name, name))
-        run("rm /tmp/{}".format(file_name))
-        run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(name))
+        put(archive_path, '/tmp/')
+        split_slash = archive_path.split("/")[-1]
+        remove_tgz= split_slash.split(".")[0]
+        directory = '/data/web_static/releases/'
+        sudo('mkdir -p {}{}'.format(directory, remove_tgz))
+        sudo('tar -xzf /tmp/{0}.tgz -C {1}{0}'.format(remove_tgz, directory))
+        sudo('rm /tmp/{}.tgz'.format(remove_tgz))
+        sudo('mv {0}{1}/web_static/* {0}{1}'.format(directory, remove_tgz))
+        sudo('rm -rf {}{}/web_static'.format(directory, remove_tgz))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}{}\
+                /data/web_static/current'.format(directory, remove_tgz))
         return True
-    except:
+    except Exception as e:
         return False
-
 
 def deploy():
     """Creates and distributes an archive to your web servers"""
-    archive_path = do_pack()
-    if archive_path is None:
+    arch_file = do_pack()
+    if arch_file is None:
         return False
-    return do_deploy(archive_path)
+    return do_deploy(arch_file)
---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def webapp_start():
+    """a function that returns a string"""
+
+    return "{}".format("Hello HBNB!")
+
+@app.route('/hbnb',strict_slashes=False)
+def hbnb():
+    """a function that returns a string"""
+
+    return "{}".format("HBNB")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=None)
