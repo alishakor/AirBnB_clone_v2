1ab00a6c64e175e4546b4e735e87c47530032637
alishakor <aliuokeowo@gmail.com>
2023-05-21 00:23:36 +0100

    adding a script that display a html number template and type:odd or even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+
+from flask import Flask, escape, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def webapp_start():
+    """a function that returns a string"""
+
+    return "{}".format("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """a function that returns a string"""
+
+    return "{}".format("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """a function that returns a string"""
+
+    if "_" in text:
+        updated_text = text.replace("_", " ")
+        return "C {}".format(escape(updated_text))
+    return "C {}".format(escape(text))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text="is_cool"):
+    """a function that returns a string"""
+
+    if "_" in text:
+        updated_word = text.replace("_", " ")
+        return "Python {}".format(escape(updated_word))
+    return "Python {}".format(escape(text))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """a function that displays a number"""
+
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_template(n):
+    """a function that displays a html template"""
+
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_type(n):
+    """ a function that display a html page with number type"""
+
+    if n % 2 == 0:
+        numb = 'even'
+    else:
+        numb = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, numb=numb)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=None)
