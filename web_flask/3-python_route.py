02158f9c08fe1b7c11c4785612c83efd2e410689
alishakor <aliuokeowo@gmail.com>
2023-05-20 18:27:22 +0100

    a script that displays text and number

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def webapp_start():
+    """a function that returns a string"""
+
+    return "{}".format("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """a function that returns a string"""
+
+    return "{}".format("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """a function that returns a string"""
+
+    if "_" in text:
+        updated_text = text.replace("_", " ")
+        return "C {}".format(escape(updated_text))
+    return "C {}".format(escape(text))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text="is_cool"):
+    """a function that returns a string"""
+
+    if "_" in text:
+        updated_word = text.replace("_", " ")
+        return "Python {}".format(escape(updated_word))
+    return "Python {}".format(escape(text))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=None)
